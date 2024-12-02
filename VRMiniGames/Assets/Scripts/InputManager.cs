189bdb93b7ba2c5f8123e495139dabb08441a01e
Lee Seo Young <seoyoung.john@gm.gist.ac.kr>
2024-12-02 20:58:48 +0900

    Update Tactile & Character (#16)
    * Update. character
    
    * Update. Character move, pose mapping
    
    * Update. vr kps normalize
    
    * Update. use vr kps
    
    * update. foot keypoint
    
    * update. tactile character, OVR camera prefab
    
    * Fix. character move by keyboard

--- VRMiniGames/Assets/Scripts/InputManager.cs
+++ VRMiniGames/Assets/Scripts/InputManager.cs
@@ -2,41 +2,69 @@ using Oculus.Interaction.Input;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class InputManager : MonoBehaviour
 {
+    //Moving speed
     public float speed = 1.0f;
 
-    private Rigidbody myRigid;
+    //Currnet action from tactile
+    private string cur_action;
+
+    // Variables for moving direction
+    public Transform hmd_transform;
+    private Vector3 front_direction = Vector3.forward;
+    private bool move_only_front = false;
 
 
-    // Start is called before the first frame update
-    void Start()
+    public TMP_Text action_text;
+    //public Transform character;
+
+    //private void Start()
+    //{
+    //    Vector3 character_pos = character.position;
+    //    character_pos.y = 1.3f;
+    //    transform.position = character_pos;
+    //}
+
+    //Socket communication evnent listener functions
+    private void OnEnable()
     {
-        myRigid = GetComponent<Rigidbody>();
+        PythonSocketClient.OnDataReceived += UpdatePythonAction;
     }
 
-    // 이벤트 리스트에 등록 및 해제
+    private void OnDisable()
+    {
+        PythonSocketClient.OnDataReceived -= UpdatePythonAction;
+    }
+    void UpdatePythonAction(ModelOutputData data)
+    {
+        this.cur_action = data.GetAction();
+        print($"Action: {cur_action}");
+    }
 
-
-    // Update is called once per frame
     void Update()
     {
         Move();
+        action_text.text = cur_action;
     }
-    
+
+
     void Move()
     {
-        //float _move_dir_X = Input.GetAxisRaw("Horizontal");
-        //float _move_dir_Z = Input.GetAxisRaw("Vertical");
-
-        //Vector3 _move_horizontal = transform.right * _move_dir_X;
-        //Vector3 _move_vertical = transform.forward * _move_dir_Z;
-
-        //Vector3 _velocity = (_move_horizontal + _move_vertical).normalized * speed;
-
-        //myRigid.MovePosition(transform.position + _velocity * Time.deltaTime);
-
+        if (cur_action == "walk")
+        {   
+            if (move_only_front)
+            {
+                transform.position = front_direction * speed * Time.deltaTime;
+            }
+            else
+            {
+                Vector3 hmd_front = hmd_transform.forward;
+                hmd_front.y = 0f;
+                transform.position += hmd_front * speed * Time.deltaTime;
+            }
+        }
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(-speed * Time.deltaTime, 0, 0);
@@ -53,7 +81,24 @@ public class InputManager : MonoBehaviour
         {
             transform.Translate(0, 0, -speed * Time.deltaTime);
         }
-        
     }
+
+    //Setters
+    public void SetSpeed(float speed)
+    {
+        this.speed = speed;
+    }
+
+    public void SetFront(Vector3 front)
+    {
+        this.front_direction = front;
+    }
+
+    public void SetOnlyMovingFront(bool move_front)
+    {
+        this.move_only_front = move_front;
+    }
+
+    //
 
 }
--- VRMiniGames/Assets/Scripts/PoseMapping.cs
+++ VRMiniGames/Assets/Scripts/PoseMapping.cs
@@ -1,57 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-[System.Serializable]
-public class VRMap
-{
-    public Transform vr_target;
-    public Transform ik_target;
-    public Vector3 tracking_pos_offset;
-    public Vector3 tracking_rotation_offset;
-    public void Map()
-    {
-        ik_target.position = vr_target.TransformPoint(tracking_pos_offset);
-        ik_target.rotation = vr_target.rotation * Quaternion.Euler(tracking_rotation_offset);
-    }
-}
-
-[System.Serializable]
-public class PoseMap
-{
-    //public Vector3 pose_target;
-    public Transform IK_target;
-    //public Transform IK_Child;
-    public Vector3 tracking_pos_offset;
-    //public Vector3 tracking_rotation_offset;
-    public void Map(Vector3 pose_target)
-    {
-        IK_target.localPosition = pose_target + tracking_pos_offset;
-    }
-
-}
-
+using UnityEngine.TextCore.Text;
 
 
 public class PoseMapping : MonoBehaviour
 {
     /*
-     KEYPOINTS = [
-    'nose', #0
-    'left_shoulder', #1
-    'right_shoulder', #2
-    'left_elbow', #3
-    'right_elbow',#4
-    'left_wrist',#5
-    'right_wrist',#6
-    'left_hip_extra',#7
-    'right_hip_extra',#8
-    'left_knee',#9
-    'right_knee',#10
-    'left_ankle',#11
-    'right_ankle',#12
-    'left_bigtoe',#13
-    'right_bigtoe',#14
+
 ]*/
     private Vector3[] python_model_keypoints;
 
@@ -60,16 +16,19 @@ public class PoseMapping : MonoBehaviour
 
 
     [Range(0, 1)]
-    public float turn_smoothness = 0.1f;
+    public float turn_smoothness = 0.1f; 
+    public float move_smoothness = 0.1f;
 
     public Vector3 head_body_pos_offset;
     public float head_body_yaw_offset;
 
-
+    //VR keypoints mapping
     public VRMap head;
     public VRMap left_hand;
     public VRMap right_hand;
 
+
+    //Tactile keypoints mapping
     public PoseMap left_shoulder;
     public PoseMap right_shoulder;
     public PoseMap left_hip;
@@ -81,9 +40,13 @@ public class PoseMapping : MonoBehaviour
     public PoseMap left_toe;
     public PoseMap right_toe;
 
-
-    public Animator animator;
-
+    //Calibration vars
+    private bool is_calibrate_done = false;
+    private Vector3 accumulatedVRPosition; 
+    private Vector3 accumulatedCenterPosition;
+    private Vector3 initialCharacterPosition; 
+    private Vector3 initialVRPosition; 
+    private int sampleCount;
 
     private void Start()
     {
@@ -91,17 +54,74 @@ public class PoseMapping : MonoBehaviour
         python_model_keypoints = new Vector3[10];
 
         pose_maps = new PoseMap[10] {
-            //nose,
             left_shoulder, right_shoulder,
-            //left_elbow, right_elbow,
-            //left_wrist, right_wrist,
             left_hip, right_hip,
             left_knee, right_knee,
             left_ankle, right_ankle,
             left_toe, right_toe
         };
 
+
+        StartCoroutine(StartCalibration(2.0f));
+
     }
+    private void LateUpdate()
+    {
+        UpdateVRKeypoints();
+        if (is_calibrate_done)
+        {
+            ApplyPythonKeypoints();
+            //UpdateVRKeypoints();
+            UpdateBodyTransform();
+        }
+    }
+
+
+    IEnumerator StartCalibration(float duration)
+    {
+        accumulatedVRPosition = Vector3.zero;
+        sampleCount = 0;
+
+        Debug.Log("Calibration started...");
+
+
+        float elapsedTime = 0.0f;
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+
+
+            accumulatedVRPosition += head.vr_target.position;
+
+            accumulatedCenterPosition += (head.vr_target.position + left_hand.vr_target.position + right_hand.vr_target.position) / 3.0f;
+            sampleCount++;
+
+
+            yield return null;
+        }
+
+
+        CompleteCalibration();
+    }
+
+    void CompleteCalibration()
+    {
+
+        initialVRPosition = accumulatedVRPosition / sampleCount;
+
+
+        //initialCharacterPosition = accumulatedCenterPosition / sampleCount;
+        initialCharacterPosition = accumulatedVRPosition / sampleCount;
+        initialCharacterPosition.y = 0f;
+
+        transform.position = initialCharacterPosition;
+        is_calibrate_done = true;
+
+        Debug.Log("Calibration completed.");
+        Debug.Log($"Initial VR Position: {initialVRPosition}");
+        Debug.Log($"Initial Character Position: {initialCharacterPosition}");
+    }
+
 
     private void OnEnable()
     {
@@ -114,66 +134,65 @@ public class PoseMapping : MonoBehaviour
     }
 
 
-    private Vector3[] ConvertArrayToVec3(float[][] array)
-    {
-        Vector3[] vectors = new Vector3[array.Length];
-        for (int i = 0; i < array.Length; i++)
-            vectors[i] = new Vector3(array[i][0], array[i][2], array[i][1]);
 
-        return vectors;
-    }
-
-    void UpdatePythonKeypoints(ModelOutputData data)
+    private void UpdatePythonKeypoints(ModelOutputData data)
     {
         python_model_keypoints = data.GetKeypoints();
-
-        //Debug.Log($"현재 오른 발 위치:{python_model_keypoints[14]}");
-
-
     }
 
-    private void LateUpdate()
+
+    public float[][] GetVRPosition()
     {
-        ApplyPythonKeypoints();
-        UpdateVRKeypoints();
+        Transform character_center = this.transform;
+
+        Vector3 head_normalize = character_center.InverseTransformPoint(head.vr_target.position);
+        Vector3 left_hand_normalize = character_center.InverseTransformPoint(left_hand.vr_target.position);
+        Vector3 right_hand_normalize = character_center.InverseTransformPoint(right_hand.vr_target.position);
+
+        float[] head_normalize_f = new float[3] { head_normalize.x, head_normalize.z, head_normalize.y };
+        float[] right_normalize_f = new float[3] { left_hand_normalize.x, left_hand_normalize.z, left_hand_normalize.y };
+        float[] left_normailze_f = new float[3] { right_hand_normalize.x, right_hand_normalize.z, right_hand_normalize.y };
+        float[][] vr_pos_data = new float[][] {head_normalize_f, left_normailze_f, right_normalize_f};
+
+        return vr_pos_data;
     }
+
 
     private void ApplyPythonKeypoints()
     {
-        //Vector3 character_pos = new Vector3(
-        //    (python_model_keypoints[11].x + python_model_keypoints[12].x) / 2,
-        //    (python_model_keypoints[11].y + python_model_keypoints[12].y) / 2,
-        //    (python_model_keypoints[11].z + python_model_keypoints[12].z) / 2);
-        //transform.position = character_pos;
-
-        //float yaw = head.vr_target.eulerAngles.y;
-        //transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(transform.eulerAngles.x, yaw, transform.eulerAngles.z), turn_smoothness);
-
-        //nose.Map(python_model_keypoints[0]);
-        //left_shoulder.Map(python_model_keypoints[1]);
-        //right_shoulder.Map(python_model_keypoints[2]);
-        //left_wrist.Map(python_model_keypoints[5]);
-        //right_wrist.Map(python_model_keypoints[6]);
-        //left_hip.Map(python_model_keypoints[7]);
-        //right_hip.Map(python_model_keypoints[8]);
-        //left_ankle.Map(python_model_keypoints[11]);
-        //right_ankle.Map(python_model_keypoints[12]);
-
         for (int i = 0; i < python_model_keypoints.Length; i++)
         {   
-            pose_maps[i].Map(python_model_keypoints[i]);
+            pose_maps[i].Map(python_model_keypoints[i], this.transform);
         }
     }
 
+
     void UpdateVRKeypoints()
     {
-        transform.position = head.ik_target.position + head_body_pos_offset;
-        float yaw = head.vr_target.eulerAngles.y;
-        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(transform.eulerAngles.x, yaw, transform.eulerAngles.z), turn_smoothness);
-
         head.Map();
         left_hand.Map();
         right_hand.Map();
     }
 
+   
+    void UpdateBodyTransform()
+    {
+
+        Vector3 character_center = (python_model_keypoints[2] + python_model_keypoints[3]) / 2.0f;
+        character_center.y = 0f;
+
+
+        Vector3 vr_move_offset = head.vr_target.position - initialVRPosition;
+        vr_move_offset.y = 0f;
+
+
+        Vector3 final_body_pos = initialCharacterPosition + vr_move_offset + character_center + head_body_pos_offset; ;
+
+
+        //transform.position = Vector3.Lerp(transform.position, final_body_pos, Time.deltaTime * move_smoothness);
+        transform.position = final_body_pos; 
+
+        float yaw = head.vr_target.eulerAngles.y;
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(transform.eulerAngles.x, yaw, transform.eulerAngles.z), turn_smoothness);
+    }
 }
