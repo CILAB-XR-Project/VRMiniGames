b005d7d911ec4723e296f386648313afce66c2d2
wiadiwon <seoyoung.john@gm.gist.ac.kr>
2024-12-02 20:49:28 +0900

    Fix. character move by keyboard

--- VRMiniGames/Assets/Scripts/InputManager.cs
+++ VRMiniGames/Assets/Scripts/InputManager.cs
@@ -65,22 +65,22 @@ public class InputManager : MonoBehaviour
                 transform.position += hmd_front * speed * Time.deltaTime;
             }
         }
-        //if (Input.GetKey(KeyCode.A))
-        //{
-        //    transform.Translate(-speed * Time.deltaTime, 0, 0);
-        //}
-        //else if (Input.GetKey(KeyCode.D))
-        //{
-        //    transform.Translate(speed * Time.deltaTime, 0, 0);
-        //}
-        //else if (Input.GetKey(KeyCode.W))
-        //{
-        //    transform.Translate(0, 0, speed * Time.deltaTime);
-        //}
-        //else if (Input.GetKey(KeyCode.S))
-        //{
-        //    transform.Translate(0, 0, -speed * Time.deltaTime);
-        //}
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -speed * Time.deltaTime);
+        }
     }
 
     //Setters
